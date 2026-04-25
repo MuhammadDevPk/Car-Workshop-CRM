5213d7ea014815b6dd4226cc272976707c409f75
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-04-25 17:30:41 +0500

    added Request

---
+++ src/Core/Request.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Core;
+
+class Request
+{
+    public function getUri()
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+    public function getMethod()
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+}
