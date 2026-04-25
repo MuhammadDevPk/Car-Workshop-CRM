ef608279f520386221aab07d4650a7788f6a60f2
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-04-25 21:50:33 +0500

    getBody() that returns payload of Request or URL

--- src/Core/Request.php
+++ src/Core/Request.php
@@ -5,10 +5,30 @@ class Request
 {
     public function getUri()
     {
+        // returns url path (e.g., /customers) 
         return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
     public function getMethod()
     {
+        // returns request method (GET, POST, PUT, DELETE, etc.)
         return $_SERVER['REQUEST_METHOD'];
+    }
+
+    public function getBody()
+    {
+        $data = [];
+
+        if ($this->getMethod() === 'GET') {
+            foreach ($_GET as $key => $value) {
+                $data[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+
+        if ($this->getMethod() === 'POST') {
+            foreach ($_POST as $key => $value) {
+                $data[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+        return $data;
     }
 }
