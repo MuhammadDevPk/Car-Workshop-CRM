b2bd1a2184bca5784e6627b118b2b7e6751f542e
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-04-26 21:02:57 +0500

    Create Router class

---
+++ src/Core/Router.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Core;
+
+class Router
+{
+    protected $routes = [];
+
+    // Get Router
+    public function get($uri, $controller)
+    {
+        $this->routes['GET'][$uri] = $controller;
+    }
+
+    // Post Router
+    public function Post($uri, $controller)
+    {
+        $this->routes['POST'][$uri] = $controller;
+    }
+
+    // Logic to find match
+    public function resolve(Request $request)
+    {
+        $path = $request->getUri();
+        $method = $request->getMethod();
+
+        $callback = $this->routes[$method][$path] ?? null;
+
+        if ($callback === false) {
+            http_response_code(404);
+            die("404 - Page Not Found");
+        }
+
+        // Handle "Controller@method"
+        if (is_string($callback)) {
+            [$controllerName, $methodName] = explode('@', $callback);
+            $controllerName = "App\\Controllers\\" . $controllerName;
+
+            $controllerName = new $controllerName();
+            return $controllerName->$methodName();
+        }
+
+        return call_user_func($callback);
+    }
+}
